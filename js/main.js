bd4d3fe67746d3740e9c7e5477ffb1e9694c37c5
fedeblancoo <fedeblanco94@hotmail.com>
2022-09-19 22:33:47 -0300

    Eventos-DOM

--- js/main.js
+++ js/main.js
@@ -1,34 +1,81 @@
 let inventario = [
   {
-    id: 1, nombre: "Abrigo", precio: 5000
+    id: 1, nombre: "Abrigo", precio: 5000, imagen: "./img/abrigo.jpg"
   },
   {
-    id: 2, nombre: "Borcegos", precio: 4100
+    id: 2, nombre: "Borcegos", precio: 4100, imagen: "./img/borcegos.jpg"
   },
   {
-    id: 3, nombre: "Cartera", precio: 2500
+    id: 3, nombre: "Cartera", precio: 2500, imagen: "./img/cartera.jpg"
+  },
+  {
+    id: 4, nombre: "Chaleco", precio: 3500, imagen: "./img/chaleco.jpg"
+  },
+  {
+    id: 5, nombre: "Falda", precio: 3200, imagen: "./img/falda.jpg"
+  },
+  {
+    id: 6, nombre: "Top", precio: 3700, imagen: "./img/top.jpg"
   }
 ];
 
+let total = 0
 
-  let prenda = Number (prompt ("¿Qué prenda vas a comprar hoy? Escribe el numero de la opcion deseada: 1-abrigo 2-borcegos 3-cartera"))
+let carrito = [];
 
-  let prendaElegida = inventario.find((e) => e.id === prenda)
+const divMain = document.getElementById ('container')
 
-  console.log(prendaElegida)
+const divCarrito = document.getElementById('carrito')
 
-  let total = 0
 
-  
-  let cantidad = Number (prompt ("Número de prendas a llevar:"));
+//TARJETA DE PRODUCTOS EN EL HTML
 
-  armarPedido (prenda, cantidad)
+inventario.forEach ((elem) => {
+  const divProductos = document.createElement('div')
+  divProductos.classList.add("card", "col-sm-6", "col-lg-4");
+  divProductos.innerHTML = `
+    <img src="${elem.imagen}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${elem.nombre}</h5>
+      <p class="card-text">$${elem.precio}.-</p>
+      <a href="#" class="btn btn-primary" id="agregar${elem.id}">Agregar al carrito</a>
+    </div>`
 
-  
-  function armarPedido (indice, cantidad) {
-    total = prendaElegida.precio * cantidad
-    if (indice === prendaElegida.id){
-      console.log(`Pedido: ${prendaElegida.nombre}. valor: $ ${prendaElegida.precio} . Cantidad: ${cantidad} Total:${total}`)
-    }
-  }
+  divMain.appendChild(divProductos)
 
+  const boton = document.getElementById(`agregar${elem.id}`)
+
+  boton.addEventListener("click", () => { 
+    buscarProducto(elem.id)
+  })
+})
+
+// BUSQUEDA DEL ELEMENTE Y PUSH AL CARRITO 
+
+const buscarProducto = (idProd) => {
+  const elementoClickeado = inventario.find((elem) => elem.id === idProd)
+
+  carrito.push(elementoClickeado)
+
+  console.log(carrito)
+
+  agregarAlCarrito ()
+}
+
+//CREAR DIVS DE ELEMENTO EN EL CARRITO
+
+const agregarAlCarrito = () => {
+  divCarrito.innerHTML = ""
+
+  carrito.forEach((elem) => {
+    const divProductosCarrito = document.createElement('div')
+    divProductosCarrito.innerHTML = `
+    <p>Nombre:${elem.nombre}</p>
+    <p>Precio:${elem.precio}</p>
+    <p id="boton-cantidad">Cantidad:<span>${elem.cantidad}</span> </p>
+    <p>Subtotal:</p>`;
+
+    divCarrito.appendChild(divProductosCarrito)
+  })
+
+}
